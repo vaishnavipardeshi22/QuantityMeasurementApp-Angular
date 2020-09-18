8e480bb9e278f2e47005272291cf6a8e51165c80
vaishnavipardeshi22@gmail.com <vaishnavipardeshi22@gmail.com>
2020-09-18 23:01:02 +0530

    [Vaishanvi] ADD : Header Component for Quantity Measurement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './component/home/home.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeaderComponent } from './component/home/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
